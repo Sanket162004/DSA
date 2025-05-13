435a0745023c96b830894da494b858f2c835a39d
Sanket162004 <snnakhate2004@gmail.com>
2025-05-13 19:15:01 +0300

    Time: 43 ms (10.57%) | Memory: 49.7 MB (8.73%) - LeetSync

---
+++ 51-n-queens/n-queens.cpp
@@ -0,0 +1,39 @@
+class Solution {
+private:
+    bool CanPlace(int row,int col,vector<string>board,int n){
+        int tr=row,tc=col;
+        while(row>=0 && col>=0) if(board[row--][col--]=='Q') return false;
+
+        row=tr,col=tc;
+        while(col>=0) if(board[row][col--]=='Q') return false;
+
+        col=tc;
+        while(row<n && col>=0) if(board[row++][col--]=='Q') return false;
+        return true;
+    }
+
+    void solve(int col,vector<vector<string>>& res,vector<string>& board,int n){
+        if(col==n){
+            res.push_back(board);
+            return;
+        }
+        for(int row=0;row<n;row++){
+            if(CanPlace(row,col,board,n)){
+                board[row][col]='Q';
+                solve(col+1,res,board,n);
+                board[row][col]='.';
+            }
+        }
+    }
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>res;
+        vector<string>board(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            board[i]=s;
+        }
+        solve(0,res,board,n);
+        return res;
+    }
+};
