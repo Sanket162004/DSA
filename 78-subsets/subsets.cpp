8fa5ffaebac215d1cf17c3d33b8301dd3f10dffb
Sanket162004 <snnakhate2004@gmail.com>
2025-06-02 13:46:00 +0300

    Time: 4 ms (12.27%) | Memory: 15.7 MB (12.00%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
-    void subsequence(vector<int>& nums,int index,int n,vector<vector<int>>&ans,vector<int>temp){
-        if(index==n){
+    void solve(vector<int>& nums,int i,int n,vector<vector<int>>&ans,vector<int>temp){
+        if(i>=n){
             ans.push_back(temp);
-            return ;
+            return;
         }
+        // not take 
+        solve(nums,i+1,n,ans,temp);
+        // take
 
-        //no
-        subsequence(nums,index+1,n,ans,temp);
-        //yes
-        temp.push_back(nums[index]);
-        subsequence(nums,index+1,n,ans,temp);
+        temp.push_back(nums[i]);
+
+        solve(nums,i+1,n,ans,temp);
 
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-
         int n=nums.size();
-
         vector<vector<int>>ans;
-
         vector<int>temp;
-
-        subsequence(nums,0,n,ans,temp);
+        int i=0;
+        solve(nums,i,n,ans,temp);
         return ans;
-
-
-        
     }
 };
