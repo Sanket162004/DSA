d8826b47ec56715fb56a86e224a1d0aba512308c
Sanket162004 <snnakhate2004@gmail.com>
2025-06-12 14:34:09 +0300

    Time: 13 ms (24.89%) | Memory: 17.5 MB (9.73%) - LeetSync

--- 47-permutations-ii/permutations-ii.cpp
+++ 47-permutations-ii/permutations-ii.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-    void solve(unordered_map<int,int>&mp,int n,vector<int>&temp,vector<vector<int>>&ans){
-        if(temp.size()==n){
-            ans.push_back(temp);
-            return ;
-        }
-
-        for(auto &[num,count]:mp){
-            if(count==0){
-                continue;
-            }
-
-            temp.push_back(num);
-            mp[num]--;
-
-            solve(mp,n,temp,ans);
-
-            temp.pop_back();
-            mp[num]++;                                                      
-
-        }
+void solve(int n,vector<int>&temp,unordered_map<int,int>mp,vector<vector<int>>&ans){
+    if(temp.size()==n){
+        ans.push_back(temp);
+        return;
     }
+
+    for(auto &[num,count]:mp){
+        if(count==0){
+            continue;
+        }
+
+        temp.push_back(num);
+        mp[num]--;
+
+
+        solve(n,temp,mp,ans);
+
+        temp.pop_back();
+        mp[num]++;
+
+        
+    }
+}
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         int n=nums.size();
-
-        vector<vector<int>>ans;
 
         unordered_map<int,int>mp;
 
         for(auto &num:nums){
             mp[num]++;
         }
+
+        vector<vector<int>>ans;
         vector<int>temp;
 
-        solve(mp,n,temp,ans);
+        int i=0;
+
+
+        solve(n,temp,mp,ans);
         return ans;
     }
 };
