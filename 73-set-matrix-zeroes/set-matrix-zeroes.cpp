91ff9b44f63e43ba93c8f81412cd0f2395e138c9
Sanket162004 <snnakhate2004@gmail.com>
2025-06-16 17:25:18 +0300

    Time: 4 ms (6.02%) | Memory: 18.5 MB (78.01%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        
-        int row=matrix.size();// it will give you no of rows
-        int col=matrix[0].size();// it will give you no of columns
+        int row=matrix.size();
+        int col=matrix[0].size();
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
                 if(matrix[i][j]==0){
-                    for(int k=0;k<col;k++){
-                       if (matrix[i][k] != 0 && matrix[i][k] != -99999 ) {
-                            matrix[i][k] = -99999;
+                    // pehle col ko -1 se assign kar dete hai 
+                    for(int k=0;k<row;k++){
+                        if(matrix[k][j]!=0 && matrix[k][j]!=-999999){
+                            matrix[k][j]=-999999;
                         }
                     }
-                    for(int k=0;k<row;k++){
-                        if (matrix[k][j] != 0  && matrix[k][j] != -99999 ) {
-                            matrix[k][j] = -99999;
+                    // ab row ke pure element zero kar dete 
+                    for(int k=0;k<col;k++){
+                        if(matrix[i][k]!=0 && matrix[i][k]!=-9999999){
+                            matrix[i][k]=-999999;
                         }
                     }
                 }
@@ -24,13 +25,11 @@ public:
 
         for(int i=0;i<row;i++){
             for(int j=0;j<col;j++){
-                if(matrix[i][j]== -99999){
+                if(matrix[i][j]==-999999){
                     matrix[i][j]=0;
                 }
             }
         }
-
-
-
+        
     }
 };
