c4a9bb94d835177012650187917f283abb3d5603
Sanket162004 <snnakhate2004@gmail.com>
2025-06-16 04:53:12 +0300

    Time: 34 ms (91.78%) | Memory: 45.3 MB (70.31%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
+        unordered_map<int,int>mp;
+        mp[0]=1;
+        int count=0;
+        int sum=0;
 
-        vector<int>prefixsum(n);
+        for(int i =0;i<nums.size();i++){
+            sum=sum+nums[i];
+            
+            if(mp.find(sum-k)!=mp.end()){
+                count=count+mp[sum-k];
+                mp[sum]++;
+               
+               
 
-        prefixsum[0]=nums[0];
-
-        for(int i=1;i<n;i++){
-            prefixsum[i]=prefixsum[i-1]+nums[i];
-        }
-
-        int cnt=0;
-
-        for(int j=0;j<n;j++){
-            if(prefixsum[j]==k){
-                cnt++;
             }
-            int i=0;
-            for ( i = 0; i < j; i++) {
-                if (prefixsum[j] - prefixsum[i] == k) {
-                    cnt++;
-                }
+            else{
+                
+                
+                
+                mp[sum]++;
             }
             
-
-
+                
+            
         }
-        return cnt;
+        return count;
     }
 };
