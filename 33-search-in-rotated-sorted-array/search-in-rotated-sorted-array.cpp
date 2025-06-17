ae7c8c48a7aed7ab00d9eaceeb62491b64b8e38e
Sanket162004 <snnakhate2004@gmail.com>
2025-06-17 12:59:31 +0300

    Time: 0 ms (100.00%) | Memory: 15.1 MB (66.56%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,35 +1,58 @@
 class Solution {
 public:
-    int search(vector<int>& arr, int target) {
-        int n=arr.size();
-        int low=0;
-        int high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(arr[mid]==target){
-                return mid;
-            }
-            // left stored part
-            if(arr[low]<=arr[mid]){
-                if(arr[low]<=target && target<=arr[mid]){
-                    high=mid-1;
-                }
-                else{
-                    low=mid+1;
-                }
-            }
+   
+    int bs(vector<int>& nums,int start,int end,int target){
+        
+       while(start<=end){
+          int mid=start+(end-start)/2;
+          if(nums[mid]==target){
+            return mid;
+        }
+        if(nums[mid]>target){
+            end=mid-1;
+        }
+        else{
+            start=mid+1;
+        }
+      }
+      return -1;
+        
+    }
+    int search(vector<int>& nums, int target) {
+        int n=nums.size();
 
-            //right stored part
+        int l=0;
+
+        int r=n-1;
+
+        while(l<r){
+            int mid=l+(r-l)/2;
+
+            
+            if(nums[mid]>nums[r]){
+                l=mid+1;
+            }
             else{
-                if(arr[mid]<=target && target<=arr[high]){
-                    low=mid+1;
-
-                }
-                else{
-                    high =mid -1;
-                }
+                r=mid;
             }
         }
-        return -1;
+
+        int pivote=r;
+
+
+
+        int first_half=bs(nums,0,pivote-1,target);
+        int second_half=bs(nums,pivote,n-1,target);
+
+
+        if(first_half!=-1){
+            return first_half;
+        }
+        else{
+            return second_half;
+        }
+            
+
+
     }
 };
