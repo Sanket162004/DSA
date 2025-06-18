67d2280abd313392f4e5833d9617c2eb33893227
Sanket162004 <snnakhate2004@gmail.com>
2025-06-18 17:32:15 +0300

    Time: 19 ms (7.43%) | Memory: 49.3 MB (6.54%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
     string removeOuterParentheses(string s) {
-        string ans="";
-        int start=0;
+        int n=s.length();
         stack<char>st;
+        string ans="";
 
-        for(int i=0;i<s.length();i++){
+        int start=0;
+
+        for(int i=0;i<n;i++){
             
-                if(s[i]=='('){
-                    st.push(s[i]);
-                }
-                else{
-                    st.pop();
+            if(s[i]=='('){
+                st.push(s[i]);
+            }
+            else{
+                st.pop();
 
-                    
-                }
+            }
 
-                if(st.empty()==1){
-                    ans=ans+s.substr(start+1,i-start-1);
-                    start=i+1;
-                }
+            if(st.empty()==1){
+              ans=ans + s.substr(start+1,i-start-1);
+              start=i+1;
+            }
 
-                
-            
-               
         }
 
         return ans;
