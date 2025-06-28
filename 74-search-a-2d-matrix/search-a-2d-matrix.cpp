1075f7495074bc398cc511568c3a89603532fcc8
Sanket162004 <snnakhate2004@gmail.com>
2025-06-28 14:28:14 +0300

    Time: 0 ms (100.00%) | Memory: 13.4 MB (41.94%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-
         int row=matrix.size();
         int col=matrix[0].size();
 
-       for(int i=0;i<row;i++){
-
-        int l=0;
-
-        int r=col-1;
-
-        while(l<=r){
-
-            int mid=l+(r-l)/2;
-
-            if(matrix[i][mid]==target){
+        if(row==1){
+            if(matrix[0][0]==target){
                 return true;
-            }
-            if(matrix[i][mid]>target){
-                r=mid-1;
-            }
-            else{
-                l=mid+1;
             }
         }
 
+        for(int i=0;i<row;i++){
 
-       }
+            int l=0;
+            int r=col-1;
 
-       return false;
-        
+            while(l<=r){
+                int mid=l+(r-l)/2;
+
+                if(matrix[i][mid]==target){
+                    return true;
+
+                }
+                if(matrix[i][mid]<target){
+                    l=mid+1;
+                }
+                else{
+                    r=mid-1;
+                }
+            }
+        }
+
+        return false;
     }
 };
