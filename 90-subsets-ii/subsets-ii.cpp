0682f3bdce23ac5fdcf931e73ad7ecf80f9ca0de
Sanket162004 <snnakhate2004@gmail.com>
2025-06-30 13:22:20 +0300

    Time: 0 ms (100.00%) | Memory: 10.5 MB (67.91%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,19 +1,39 @@
 class Solution {
 public:
-    void solve(int idx, vector<int>& arr, vector<int>& temp, vector<vector<int>>& result) {
-        result.push_back(temp);
-        for (int i = idx; i < arr.size(); i++) {
-            if (i > idx && arr[i] == arr[i - 1]) continue; 
-            temp.push_back(arr[i]);
-            solve(i + 1, arr, temp, result);
-            temp.pop_back(); 
+
+    void solve(int index,vector<int>& nums,vector<vector<int>>&ans,vector<int>&temp,int n){
+        if(index>=n){
+            ans.push_back(temp);
+            return ;
         }
+
+
+        // aagar lena hai 
+
+        temp.push_back(nums[index]);
+        solve(index+1,nums,ans,temp,n);
+        temp.pop_back();
+
+        int i=index+1;
+
+        while(i<n && nums[i]==nums[i-1]){
+            i++;
+        }
+
+        solve(i,nums,ans,temp,n);
+
     }
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        sort(nums.begin(), nums.end()); 
-        vector<vector<int>> result;
-        vector<int> temp;
-        solve(0, nums, temp, result);
-        return result;
+        int n=nums.size();
+
+        sort(nums.begin(),nums.end());
+
+        vector<vector<int>>ans;
+        vector<int>temp;
+        int index=0;
+
+        solve(index,nums,ans,temp,n);
+
+        return ans;
     }
 };
