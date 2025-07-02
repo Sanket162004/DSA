2197c8a096756c06cf849bdea0666a03dafc79b0
Sanket162004 <snnakhate2004@gmail.com>
2025-07-02 11:25:24 +0300

    Time: 3 ms (87.92%) | Memory: 15.8 MB (40.44%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
-    int dp[21][2001];
-    int solve(vector<int>& nums, int target,int i,int x){
-        if(i==nums.size() && target!=x){
-            return 0;
-        }
-        if(target==x && i==nums.size()){
-            return 1;
-        }
-        if (dp[i][x + 1000] != -1) return dp[i][x + 1000];
-       
-        int postive=solve(nums,target,i+1,x+nums[i]);
-
-        int negative=solve(nums,target,i+1,x-nums[i]);
-
-        return dp[i][x + 1000]=postive+negative;
-    }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n=nums.size();
-        int i=0;
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,target,0,0);
+        int sum = 0;
+        for (int num : nums) sum += num;
 
-       
+        // Check if (sum + target) is even and non-negative
+        if ((sum + target) % 2 != 0 || (sum + target) < 0) return 0;
 
+        int subsetSum = (sum + target) / 2;
+        int n = nums.size();
+
+        vector<vector<int>> dp(n + 1, vector<int>(subsetSum + 1, 0));
+
+        // Initialization: ways to get sum 0 is 1 (empty subset)
+        for (int i = 0; i <= n; i++) dp[i][0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j <= subsetSum; j++) {
+                if (nums[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+
+        return dp[n][subsetSum];
     }
 };
