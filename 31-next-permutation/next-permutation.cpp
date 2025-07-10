0209154f91d71f2c02f90b5585422c8e5d3b3754
Sanket162004 <snnakhate2004@gmail.com>
2025-07-10 04:57:47 +0300

    Time: 0 ms (100.00%) | Memory: 15.8 MB (33.07%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,47 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-         next_permutation(nums.begin(),nums.end());
+        int n=nums.size();
+
+        int pivot=-1;
+
+        
+
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                pivot=i;
+                break;
+
+            }
+        }
+
+        if(pivot==-1){
+             sort(nums.begin(),nums.end());
+        }
+
+        else{
+            sort(nums.begin()+pivot+1,nums.end());
+            // swap(nums[pivot],nums[pivot+1]);
+
+            // find the right most greates element then swap it 
+
+            for(int j=pivot+1;j<n;j++){
+                if(nums[j]>nums[pivot]){
+                    swap(nums[pivot],nums[j]);
+                    break;
+                }
+            }
+
+            
+
+
+
+
+           // return nums;
+        }
+
+        
+
+
     }
 };
