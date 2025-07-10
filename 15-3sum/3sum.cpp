c4eb7b65bf611f93f6cf76fe53b98efb2ec73558
Sanket162004 <snnakhate2004@gmail.com>
2025-07-10 05:22:35 +0300

    Time: 48 ms (62.62%) | Memory: 29.2 MB (51.22%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,59 +1,57 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        
-
         int n=nums.size();
 
         vector<vector<int>>ans;
 
         sort(nums.begin(),nums.end());
-        
+
         for(int k=0;k<n-2;k++){
-            if (k > 0 && nums[k] == nums[k - 1]) continue;
+            if(k>0 && nums[k]==nums[k-1]){
+                continue;
+            }
+
             int i=k+1;
             int j=n-1;
 
-           
             int sum=0;
 
-           
-
             while(i<j){
-               sum=nums[k]+nums[i]+nums[j];
+                sum=nums[k]+nums[i]+nums[j];
 
-               if(sum==0){
-                
-                ans.push_back({nums[k],nums[i],nums[j]});
+                // tin condition sum ==0
+                // sum >0
+                //sum<0;
 
-                while(i<j && nums[j]==nums[j-1]){
+                if(sum==0){
+                    ans.push_back({nums[k],nums[i],nums[j]});
+                    // skip wali condition q ki aapne i j equal nahi rahe sake
+
+                    while(i<j&& nums[j]==nums[j-1]){
+                        j--;
+                    }
+                    while(i<j&&nums[i]==nums[i+1]){
+                        i++;
+                    }
+
+                    // ans mil gaya further check karege ki answer mil ra kya 
+
+                    i++;
                     j--;
                 }
-                 while(i<j && nums[i]==nums[i+1]){
+
+               else if(sum>0){
+                    j--;
+                }
+                else{
                     i++;
                 }
-
-                i++;
-                j--;
-
-                
-               }
-
-               if(sum>0){
-                 
-                
-                j--;
-               }
-
-               if(sum<0){
-               
-                i++;
-               }
             }
         }
 
         return ans;
 
-
+        
     }
 };
