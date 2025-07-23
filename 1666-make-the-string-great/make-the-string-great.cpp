c0d256383512c66f8ca683e2dac56bb73747ed3f
Sanket162004 <snnakhate2004@gmail.com>
2025-07-23 11:58:59 +0300

    Time: 0 ms (100.00%) | Memory: 8.5 MB (52.00%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string ans = "";
+
+        for (char ch : s) {
+            if (!ans.empty() && (abs(ans.back() - ch) == 32)) {
+                ans.pop_back();
+            } else {
+                ans.push_back(ch);
+            }
+        }
+
+        return ans;
+    }
+};
