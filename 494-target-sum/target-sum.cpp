8a27610f3022caf4116371089b4b752e7661f315
Sanket162004 <snnakhate2004@gmail.com>
2025-07-30 14:01:10 +0300

    Time: 836 ms (11.57%) | Memory: 11.3 MB (100.00%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int findTargetSumWays(vector<int>& nums, int target) {
-        int sum = 0;
-        for (int num : nums) sum += num;
-
-        // Check if (sum + target) is even and non-negative
-        if ((sum + target) % 2 != 0 || (sum + target) < 0) return 0;
-
-        int subsetSum = (sum + target) / 2;
-        int n = nums.size();
-
-        vector<vector<int>> dp(n + 1, vector<int>(subsetSum + 1, 0));
-
-        // Initialization: ways to get sum 0 is 1 (empty subset)
-        for (int i = 0; i <= n; i++) dp[i][0] = 1;
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= subsetSum; j++) {
-                if (nums[i - 1] <= j)
-                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - nums[i - 1]];
-                else
-                    dp[i][j] = dp[i - 1][j];
+    int solve(int i,int sum,vector<int>& nums, int target,int n){
+        if(i==n){
+            if(sum==target){
+                return 1;
+            }
+            else{
+                return 0;
             }
         }
 
-        return dp[n][subsetSum];
+        // 
+        int plus=solve(i+1,sum+nums[i],nums,target,n);
+        int minus=solve(i+1,sum-nums[i],nums,target,n);
+
+
+        return plus+minus;
+    }
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int i=0;
+        int n=nums.size();
+        int sum=0;
+
+        return solve(i,sum,nums,target,n);
     }
 };
