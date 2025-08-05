48b51c991825a01f94d00ac911dece51d70cfd70
Sanket162004 <snnakhate2004@gmail.com>
2025-08-05 12:00:14 +0300

    Time: 0 ms (100.00%) | Memory: 10.6 MB (97.48%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -1,31 +1,35 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    void traversal(TreeNode*root,vector<int>&ans){
-        if(root ==NULL){
-            return ;
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+
+        while (root) {
+            // Case 1: If left subtree doesn't exist
+            if (root->left == NULL) {
+                ans.push_back(root->val);  // Visit the node
+                root = root->right;        // Move to right subtree
+            }
+            else {
+                // Find the rightmost node of the left subtree
+                TreeNode* curr = root->left;
+                while (curr->right && curr->right != root) {
+                    curr = curr->right;
+                }
+
+                // Case 2: First time visiting this node, build the thread
+                if (curr->right == NULL) {
+                    curr->right = root;     // Create thread (temporary link)
+                    root = root->left;      // Move to left subtree
+                }
+                // Case 3: Already visited left subtree, remove the thread
+                else {
+                    curr->right = NULL;     // Remove the thread
+                    ans.push_back(root->val);  // Visit the node
+                    root = root->right;        // Move to right subtree
+                }
+            }
         }
 
-        
-        traversal(root->left,ans);
-        ans.push_back(root->val);
-        traversal(root->right,ans);
-    }
-    vector<int> inorderTraversal(TreeNode* root) {
-        vector<int>ans;
-
-        traversal(root,ans);
         return ans;
     }
-
 };
