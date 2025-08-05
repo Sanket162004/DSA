942eb609fb27e28e17873890dbb0dbd539a5178e
Sanket162004 <snnakhate2004@gmail.com>
2025-08-05 12:04:18 +0300

    Time: 0 ms (100.00%) | Memory: 10.8 MB (71.07%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.cpp
@@ -11,24 +11,37 @@
  */
 class Solution {
 public:
-    void traversal(TreeNode* root,vector<int>&ans){
-         if(root==NULL){
-            return ;
-         }
-
-         ans.push_back(root->val);
-         traversal(root->left,ans);
-         traversal(root->right,ans);
-
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int>ans;
+         vector<int> ans;
 
-        
-        traversal(root,ans);
+        while (root) {
+            // Case 1: If left subtree doesn't exist
+            if (root->left == NULL) {
+                ans.push_back(root->val);  // Visit the node
+                root = root->right;        // Move to right subtree
+            }
+            else {
+                // Find the rightmost node of the left subtree
+                TreeNode* curr = root->left;
+                while (curr->right && curr->right != root) {
+                    curr = curr->right;
+                }
+
+                // Case 2: First time visiting this node, build the thread
+                if (curr->right == NULL) {
+                    ans.push_back(root->val);
+                    curr->right = root;     // Create thread (temporary link)
+                    root = root->left;      // Move to left subtree
+                }
+                // Case 3: Already visited left subtree, remove the thread
+                else {
+                    curr->right = NULL;     // Remove the thread
+                     // Visit the node
+                    root = root->right;        // Move to right subtree
+                }
+            }
+        }
 
         return ans;
-
     }
-    
 };
