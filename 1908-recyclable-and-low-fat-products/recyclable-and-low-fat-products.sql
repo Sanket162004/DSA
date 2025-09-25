c0b1e70ba1ff5b1a30ad6722e48c8cde029ae672
Sanket162004 <snnakhate2004@gmail.com>
2025-09-25 20:28:32 +0530

    Time: 587 ms (21.80%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,2 +1,6 @@
 # Write your MySQL query statement below
-select product_id from products where low_fats='Y' and recyclable='y';
+--  whwen were you are using aggregate fuction then use having clause else use where clause
+
+select product_id 
+from products
+where low_fats='Y' and recyclable='Y'
