46c36d649a5e67b36b4ae3223a59fe6456750443
Sanket162004 <snnakhate2004@gmail.com>
2025-10-05 19:31:24 +0530

    Time: 334 ms (65.77%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,8 +1,8 @@
 # Write your MySQL query statement below
 
 select e1.name 
-from employee e1
-join employee e2 #aaya pe aapan self join kara rahe
-on e1.id=e2.managerId
+from  employee e1
+inner join employee e2
+on e1.id =e2.managerId
 group by e2.managerId
 having count(e2.managerId)>=5
