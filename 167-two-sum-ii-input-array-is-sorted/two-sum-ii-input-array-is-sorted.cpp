33d0b072751156a1bfbdaf466e222b0130e2fa2e
Sanket162004 <snnakhate2004@gmail.com>
2025-12-15 21:35:49 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (63.32%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        // if input arr is stored you have just have to find the target
+        // at that movement use two pointer 
+        int n=numbers.size();
+        int left=0;
+        int right=n-1;
+
+        while(left<right){
+            if(numbers[left]+numbers[right]==target){
+                return {left+1,right+1};
+            }
+            else if(numbers[left]+numbers[right]>target){
+                right--;
+            }
+            else{
+                left++;
+            }
+        }
+
+        return {-1,-1};
+
+
+    }
+};
